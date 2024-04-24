1d8493a00df9253a45a77204228c8cf5fa3d43c8
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2024-04-24 21:17:14 +0530

    Initial commit

---
+++ src/test/java/com/example/employee/EmployeeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
