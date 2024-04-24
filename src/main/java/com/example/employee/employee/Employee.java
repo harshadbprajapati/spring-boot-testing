cd036d09c2b8c1c0510b9c92daf5e1489fbeb55e
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2024-04-24 21:41:49 +0530

    feat(employee): add Employee entity

--- src/main/java/com/example/employee/employee/Employee.java
+++ src/main/java/com/example/employee/employee/Employee.java
@@ -1,2 +1,27 @@
-package com.example.employee.employee;public class Employee {
+package com.example.employee.employee;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    public Employee(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
