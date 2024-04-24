aff03253faa8667766d2987bc963056082de9d1c
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2024-04-24 21:45:01 +0530

    feat(employee): add EmployeeRepository

---
+++ src/main/java/com/example/employee/employee/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.example.employee.employee;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
+}
+
