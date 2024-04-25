1b5dc949a7fb01e4088cf6ed3d1fb07d434a6d57
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2024-04-25 06:53:56 +0530

    test(application): add test for application is loading

--- src/test/java/com/example/employee/EmployeeTestApplicationTests.java
+++ src/test/java/com/example/employee/EmployeeTestApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.employee;
 
+import com.example.employee.employee.EmployeeController;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EmployeeTestApplicationTests {
 
+	@Autowired
+	private EmployeeController employeeController;
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(employeeController).isNotNull();
 	}
 
 }
