29089c38a417216ade8678b334fedf0774f23842
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2024-04-26 09:58:23 +0530

    test(employee-service): add correct test for save employee

--- src/test/java/com/example/employee/employee/EmployeeServiceTest.java
+++ src/test/java/com/example/employee/employee/EmployeeServiceTest.java
@@ -7,6 +7,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
@@ -22,14 +25,21 @@ class EmployeeServiceTest {
     @Test
     public void Employee_Save_ReturnsEmployee() throws Exception {
         //Arrange
+        Employee oldEmployee = new Employee("Tom", "Smith");
+        oldEmployee.setId(1);
         Employee employee = new Employee("Tom", "Cruise");
-        when(employeeRepository.save(Mockito.any(Employee.class))).thenReturn(employee);
+        employee.setId(1);
+        when(employeeRepository.findById(1)).thenReturn(Optional.ofNullable(oldEmployee));
+        when(employeeRepository.save(Mockito.any(Employee.class)))
+                .thenAnswer(invocation -> invocation.getArgument(0));
 
         //Act
         Employee savedEmployee = employeeService.save(employee);
 
         //Assert
         Assertions.assertThat(savedEmployee).isNotNull();
+        assertEquals(employee.getFirstName(), savedEmployee.getFirstName());
+        assertEquals(employee.getLastName(), savedEmployee.getLastName());
     }
 
 }
