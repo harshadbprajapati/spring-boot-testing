fe9ee580169476c1621f2133b547acb68c1e5dc1
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2024-04-27 17:51:52 +0530

    test(employee-controller): add test for add employee with just checking status

--- src/test/java/com/example/employee/employee/EmployeeControllerTest.java
+++ src/test/java/com/example/employee/employee/EmployeeControllerTest.java
@@ -1,18 +1,24 @@
 package com.example.employee.employee;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -20,6 +26,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class EmployeeControllerTest {
     @MockBean
     private EmployeeService employeeService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Autowired
     MockMvc mockMvc;
@@ -35,6 +44,17 @@ class EmployeeControllerTest {
                 .andExpect(jsonPath("$", Matchers.hasSize(1)))
                 .andExpect(jsonPath("$[0].firstName", Matchers.is("Tom")))
                 .andExpect(jsonPath("$[0].lastName", Matchers.is("Cruise")));
+    }
+
+    @Test
+    public void EmployeeController_AddEmployee_ReturnsCreated() throws Exception{
+        Employee employee = new Employee("Tom", "Cruise");
+        Mockito.when(employeeService.save(ArgumentMatchers.any()))
+                .thenAnswer(invocation -> invocation.getArgument(0));
+        ResultActions response = mockMvc.perform(post("/api/v1/employees")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(employee)));
+        response.andExpect(MockMvcResultMatchers.status().isOk());
     }
 
 }
